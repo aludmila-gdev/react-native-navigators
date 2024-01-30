27e812564b4afd397b6418ed496c8b7c0690663d
Ana Ludmila <aludmila.gdev@gmail.com>
2024-01-30 16:40:25 -0300

    first commit

---
+++ src/views/TelaB.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TextoCentral from "../components/TextoCentral";
+
+export default props => (
+    <TextoCentral corFundo='#eb82c4'>
+        Tela B
+    </TextoCentral>
+)
